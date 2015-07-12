96a26d3a19494593081b8e7c3adbbc3a738bfb82
TJ Horner <me@tjhorner.com>
2015-07-11 19:20:47 -0700

    lol readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to install
+
+1. clone wherever
+
+2. make an alias to wherever app.js is because `node-gyp` is giving me a hard time right now. example: `alias build-cloud="node ~/cloud-buildcli/app.js"`
+
+This thing is really meant for internal use, so I don't need to write good code anyways.
--- app.js
+++ app.js
@@ -1,36 +1,58 @@
-var config = require('./config.json'),
-    io = require('socket.io-client'),
-    socket = io.connect('ws://' + config.server_ip + ":" + config.server_port),
-    // colors are super important
-    colors = require('colors');
+#! /usr/bin/env node
 
-socket.on("key:send", function(){
-  console.log("Authenticating...");
-  socket.emit("key:send", config.auth.password);
-});
+function build(){
+  var config = require('./config.json'),
+      io = require('socket.io-client'),
+      socket = io.connect('ws://' + config.server_ip + ":" + config.server_port),
+      // colors are super important
+      colors = require('colors');
 
-socket.on("key:success", function(){
-  console.log("Authentication success! Running build scripts...");
-  socket.emit("build:run");
-});
+  console.log("Connecting to build server " + config.server_ip + ":" + config.server_port + "...");
 
-socket.on("stdout", function(data){
-  var log = data.stdout.split("\n");
-  log.forEach(function(e){
-    if(e.trim() !== ""){
-      var tag = "[REMOTE] ".green;
-      if(data.tag)
-        var tag = "[REMOTE (".green + data.tag.cyan + ")] ".green;
-      console.log(tag + e);
-    }
+  socket.on("key:send", function(){
+    console.log("Authenticating...");
+    socket.emit("key:send", config.auth.password);
   });
-});
 
-socket.on("build:complete", function(status){
-  process.exit(status);
-});
+  socket.on("key:success", function(){
+    console.log("Authentication success! Running build scripts...");
+    socket.emit("build:run");
+  });
 
-socket.on("key:fail", function(){
-  console.log("Authentication failed. Exiting.");
-  process.exit(1);
-});
+  socket.on("stdout", function(data){
+    var log = data.stdout.split("\n");
+    log.forEach(function(e){
+      if(e.trim() !== ""){
+        var tag = "[REMOTE] ".green;
+        if(data.tag)
+          var tag = "[REMOTE (".green + data.tag.cyan + ")] ".green;
+        console.log(tag + e);
+      }
+    });
+  });
+
+  socket.on("build:complete", function(status){
+    process.exit(status);
+  });
+
+  socket.on("key:fail", function(){
+    console.log("Authentication failed. Exiting.");
+    process.exit(1);
+  });
+}
+
+if(process.argv[2]){
+  switch(process.argv[2]){
+    case 'config':
+      console.log(__dirname + "/config.json");
+      break;
+    case 'build':
+      build();
+      break;
+    default:
+      console.log("Command not recognized. Available commands are: config, build");
+      break;
+  }
+}else{
+  build();
+}
